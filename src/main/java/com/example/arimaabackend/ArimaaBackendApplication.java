9def5f8ade4fc825e3ddafca98c219a502897609
Christian Lykke <cmlykke@hotmail.com>
2026-04-16 13:22:58 +0200

    first commit

---
+++ src/main/java/com/example/arimaabackend/ArimaaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.arimaabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArimaaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArimaaBackendApplication.class, args);
+    }
+
+}

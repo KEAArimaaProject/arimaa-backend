fb7a388d547ddf34ef8d4d6a5d1fe2efbbeefee8
Christian Lykke <cmlykke@hotmail.com>
2026-04-16 14:42:57 +0200

    create a simpel test that will pass

--- src/test/java/com/example/arimaabackend/ArimaaBackendApplicationTests.java
+++ src/test/java/com/example/arimaabackend/ArimaaBackendApplicationTests.java
@@ -2,12 +2,20 @@ package com.example.arimaabackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@org.springframework.test.context.ActiveProfiles("test")
 class ArimaaBackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void test() {
+        assertTrue(1 == 1, "1 should equal 1");   // message is optional but recommended
+    }
+
 
 }

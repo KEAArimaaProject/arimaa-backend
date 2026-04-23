9770fe41222ea9fc1ea2a21e8a80033f93ef3969
AndreasRahbek <andreasrh7913@gmail.com>
2026-04-23 18:10:27 +0200

    Fixed wrong repository implementation

--- src/main/java/com/example/arimaabackend/repository/mongo/MatchMongoRepository.java
+++ src/main/java/com/example/arimaabackend/repository/mongo/MatchMongoRepository.java
@@ -1,7 +1,7 @@
 package com.example.arimaabackend.repository.mongo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.arimaabackend.model.mongo.MatchDocument;
 
-public interface MatchMongoRepository extends JpaRepository<MatchDocument, String> {}
+public interface MatchMongoRepository extends MongoRepository<MatchDocument, String> {}

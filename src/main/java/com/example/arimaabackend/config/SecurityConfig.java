3dd242997ff26831ed0422d5fcd33c44a8c49941
AndreasRahbek <andreasrh7913@gmail.com>
2026-05-25 05:13:28 +0200

    Endpoints for neo4j

--- src/main/java/com/example/arimaabackend/config/SecurityConfig.java
+++ src/main/java/com/example/arimaabackend/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.arimaabackend.config;
 
-import com.example.arimaabackend.security.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.arimaabackend.security.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
